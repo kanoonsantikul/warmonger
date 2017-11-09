7589e22d2c40457574b9dbb0f07dce0ff6a16ebf
kanoonsantikul <nitisantikul@gmail.com>
2017-11-09 21:31:14 +0700

    set world

---
+++ core/src/com/olan/warmonger/GameScreen.java
@@ -0,0 +1,50 @@
+package com.olan.warmonger;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputMultiplexer;
+import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.profiling.GLProfiler;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+
+public class GameScreen extends ScreenAdapter {
+	private SpriteBatch batch;
+
+	private FitViewport worldViewport;
+	private World world;
+
+	public GameScreen (SpriteBatch batch) {
+		this.batch = batch;
+
+		InputMultiplexer plexInput = new InputMultiplexer();
+		Gdx.input.setInputProcessor(plexInput);
+
+		worldViewport = new FitViewport(World.WIDTH, World.HEIGHT);
+		world = new World(worldViewport, batch);
+		plexInput.addProcessor(world);
+
+		GLProfiler.enable();
+	}
+
+	@Override
+	public void render (float delta) {
+		GLProfiler.reset();
+
+		Gdx.gl.glClearColor(1, 1, 1, 0);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+		world.act();
+		world.draw();
+	}
+
+	@Override
+	public void resize (int width, int height) {
+		worldViewport.update(width, height);
+	}
+
+	@Override
+	public void dispose () {
+		world.dispose();
+	}
+}
---
+++ core/src/com/olan/warmonger/World.java
@@ -0,0 +1,35 @@
+package com.olan.warmonger;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.utils.viewport.Viewport;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.Texture;
+
+public class World extends Stage {
+	public static final int WIDTH = Gdx.graphics.getWidth();
+	public static final int HEIGHT = Gdx.graphics.getHeight();
+
+	private static World world;
+	private Viewport viewport;
+
+  Texture image = new Texture("badlogic.jpg");
+
+	public World (Viewport viewport, SpriteBatch batch) {
+		super(viewport, batch);
+		this.world = this;
+		this.viewport = viewport;
+	}
+
+	public static World instance () {
+		return world;
+	}
+
+	@Override
+	public void draw () {
+		super.draw();
+    getBatch().begin();
+    getBatch().draw(image, 0, 0);
+    getBatch().end();
+  }
+}
