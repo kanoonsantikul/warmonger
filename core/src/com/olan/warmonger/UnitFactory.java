7fd91f6effc1d315e14e2a98be55ecc9fef7b3b0
Kritsana <kritsana.chai@ku.th>
2017-11-24 04:17:19 +0700

    Edit unit factory more generic. Fix bug when can't set unit

--- core/src/com/olan/warmonger/UnitFactory.java
+++ core/src/com/olan/warmonger/UnitFactory.java
@@ -4,21 +4,26 @@ import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 
 public class UnitFactory extends GameObject {
-  private Class c;
-
+  private Class unitClass;
   UnitFactoryListener listener;
 
-  public UnitFactory () {
-    this.c = c;
+  public UnitFactory (Class unitClass) {
+    this.unitClass = unitClass;
 
-    setTexture(Assets.pikemanFront);
+    if (unitClass == Unit.class) {
+      setTexture(Assets.pikemanFront);
+    }
 
     setPosition(1000f, 600f);
 
     addListener(new ClickListener () {
       public void clicked (InputEvent event, float x, float y) {
         if (listener != null) {
-          Unit unit = new Unit(Team.RED, 0, 0);
+          Unit unit = null;
+          if (getOuter().getUnitClass() == Unit.class) {
+            unit = new Unit(Team.RED, 0, 0);
+          }
+
           listener.onUnitFactoryClicked(unit);
         }
       }
@@ -27,6 +32,14 @@ public class UnitFactory extends GameObject {
 
   public void addListener (UnitFactoryListener listener) {
     this.listener = listener;
+  }
+
+  public UnitFactory getOuter() {
+    return UnitFactory.this;
+  }
+
+  public Class getUnitClass () {
+    return this.unitClass;
   }
 
   public interface UnitFactoryListener {
