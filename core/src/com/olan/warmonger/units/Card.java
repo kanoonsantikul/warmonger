d3a56969dc01174eac8e33c2242d1f2711d69d6f
Kritsana <kritsana.chai@ku.th>
2017-11-25 18:02:41 +0700

    Fix clone unit in card

--- core/src/com/olan/warmonger/units/Card.java
+++ core/src/com/olan/warmonger/units/Card.java
@@ -5,17 +5,29 @@ import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 public class Card extends GameObject {
-  private Unit prototype;
+  private Class unitClass;
   private CardListener listener;
 
-  public Card (TextureRegion cardTexture , Unit prototype) {
+  public Card (TextureRegion cardTexture , Class unitClass) {
     super(cardTexture);
-    this.prototype = prototype;
+    this.unitClass = unitClass;
 
     addListener(new ClickListener () {
       public void clicked (InputEvent event, float x, float y) {
         if (listener != null) {
-          listener.onCardClicked(Card.this.prototype.clone());
+          Unit unit = null;
+          Class[] cArg = new Class[3];
+          cArg[0] = Team.class;
+          cArg[1] = int.class;
+          cArg[2] = int.class;
+
+          try {
+            unit = (Unit)Card.this.unitClass.getDeclaredConstructor(cArg).newInstance(null, 0, 0);
+          } catch (Exception e) {
+            System.out.println(e.toString());
+          }
+
+          listener.onCardClicked(unit);
         }
       }
     });
